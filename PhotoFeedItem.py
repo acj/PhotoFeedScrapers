60e2ffe503a6f80d733700a0703bb05b6961b3da
Adam Jensen <acj@linuxguy.org>
2014-02-02 22:03:23 -0500

    PhotoFeedItem class skeleton

---
+++ PhotoFeedItem.py
@@ -0,0 +1,11 @@
+class PhotoFeedItem:
+    def __init__(self, title, caption, page_url, photo_url, pub_date, source):
+        self.title = title
+        self.caption = caption
+        self.page_url = page_url
+        self.photo_url = photo_url
+        self.pub_date = pub_date
+        self.source = source
+
+    def __init__(self):
+        pass
